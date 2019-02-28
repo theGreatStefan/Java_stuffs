4a19c835e523ce0cd60e27253013fdc3cac06014
Stefan <stefanvd44@gmail.com>
2019-02-28 16:31:28 +0200

    Added troubleSort, the second code jam problem

---
+++ gcj/codejam/troubleSort.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class troubleSort {
+	static Scanner sc;
+	static int totalSeqs = 0, seqLength = 0;
+
+	public static void main(String[] args) {
+		String filename;
+		int[] seqArr;
+
+		filename = args[0];
+		read_initial_file(filename);
+
+		for (int i=0; i < totalSeqs; i++) {
+			seqArr = read_next_seq();
+			seqArr = trouble_sort(seqArr);
+			
+			for (int j=0; j < seqArr.length; j++) {
+				System.out.print(seqArr[j]+" ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+	public static int find_index(int[] seqArr) {
+		int index = -1;
+		for (int i=0; i < seqArr.length-1; i++) {
+			if (seqArr[i] > seqArr[i+1]) {
+				index = i;
+				break;
+			}
+		}
+
+		return index;
+
+	}
+
+	public static int[] trouble_sort(int[] seqArr) {
+		boolean done = false;
+		int temp;
+
+		while (!done) {
+			done = true;
+			for (int i=0; i < (seqLength-2); i++) {
+				if (seqArr[i] > seqArr[i+2]) {
+					done = false;
+					temp = seqArr[i];
+					seqArr[i] = seqArr[i+2];
+					seqArr[i+2] = temp;
+				}
+
+			}
+		}
+		return seqArr;
+	}
+
+	public static int[] read_next_seq() {
+		String seq[];
+		int[] seqArr;
+		seqLength = Integer.parseInt(sc.nextLine());
+		seq = sc.nextLine().split(" ");
+		seqArr = new int[seqLength];
+		for (int i=0; i < seqLength; i++) {
+			seqArr[i] = Integer.parseInt(seq[i]);
+		}
+
+		return seqArr;
+	}
+
+	public static void read_initial_file(String filename) {
+		File file = new File(filename);
+		try {
+			sc = new Scanner(file);
+			totalSeqs = Integer.parseInt(sc.nextLine());
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			System.exit(0);
+		}
+		
+	}
+
+}
